74760696f87eced4b05865933013a2f9f97c1bca
Mitzi Manriquez <mimma.dfs@gmail.com>
2026-01-12 01:46:34 -0400

    feat: add about popup component, shared styles an unify page styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,16 @@
 import './About.css'
 
-function About() {
+export default function AboutPopup() {
   return (
-    <section className="about">
-      <h2 className="about__title">About</h2>
-      <p className="about__text">
-        Aquí va tu descripción / autor (componente de presentación).
-      </p>
-    </section>
-  )
-}
+    <div className="popup__form form form__about">
+      <h2 className="form__title">About me</h2>
 
-export default About
+      <p className="form__text">
+        {/* Cambia este texto por tu descripción */}
+        Hola! soy Mitzi. Soy chilena, Desarrollador full Stack en formación, buscando aprender acerca del mundo de la tecnología.
+        Dentro el bootcamp de TripleTen estoy construyendo Pocket Bestiary (Pokédex) con React y
+        Vite. Además de aprender otros como HTML, CSS y Backend como; Node.js, Express, APIs y herramientas como Git y Github.
+      </p>
+    </div>
+  );
+}
